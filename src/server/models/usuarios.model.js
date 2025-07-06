5d6b29fd84f2f50c2fd3c52e04ddf7336cd9ef16
Gbriel999 <83030359+Gbriel999@users.noreply.github.com>
2025-07-05 20:42:11 -0400

    Update usuarios.model.js

--- src/server/models/usuarios.model.js
+++ src/server/models/usuarios.model.js
@@ -1,6 +1,29 @@
 import db from '../database/db_connect.js'
+import bcrypt from 'bcrypt'
+export const register = async ({ email, pass }) => {
+  try {
+    const hashedPass = await bcrypt.hash(pass, 10) // 10 rondas de sal
+    return db('INSERT INTO usuarios (id, email, pass) VALUES (DEFAULT, $1, $2);', [email, hashedPass])
+  } catch (error) {
+    return { code: 500, message: 'Error al registrar usuario', error }
+  }
+}
 
-export const register = ({ email, pass }) => db('INSERT INTO usuarios (id, email, pass) VALUES (DEFAULT,$1,$2);',[email, pass])
-export const login = ({ email, pass }) => {
-    return db('SELECT email FROM usuarios WHERE email = $1 and pass = $2;', [email, pass])
+export const login = async ({ email, pass }) => {
+  try {
+    const result = await db('SELECT * FROM usuarios WHERE email = $1;', [email])
+    if (result.length === 0) {
+      return [] 
+    }
+
+    const user = result[0]
+    const passwordMatch = await bcrypt.compare(pass, user.pass)
+    if (!passwordMatch) {
+      return [] 
+    }
+
+    return [user] 
+  } catch (error) {
+    return { code: 500, message: 'Error al hacer login', error }
+  }
 }
